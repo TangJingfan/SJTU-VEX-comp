0118b2ef34460e97954e0589d46a26da7f71732f
Daicheng Li <not_lell@sjtu.edu.cn>
2024-11-20 23:11:50 +0800

    fix: wrong direction of LB RF wheels

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ controller Controller;
 
 // motor setup
 motor left_front_motor(PORT1, gearSetting::ratio18_1, false);
-motor right_front_motor(PORT2, gearSetting::ratio18_1, false);
-motor left_back_motor(PORT3, gearSetting::ratio18_1, true);
+motor right_front_motor(PORT2, gearSetting::ratio18_1, true);
+motor left_back_motor(PORT3, gearSetting::ratio18_1, false);
 motor right_back_motor(PORT4, gearSetting::ratio18_1, true);
 
 brain Brain;
