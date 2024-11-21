496c46d89ea936a3ae510e2bc06b1e45fd0e6d91
TangWan <tangjingfan@gmail.com>
2024-11-21 21:29:31 +0800

    refactor: add movement.cpp

--- include/movement.h
+++ include/movement.h
@@ -4,34 +4,14 @@ using namespace vex;
 
 void drive_forward(motor &left_front_motor, motor &left_back_motor,
                    motor &right_front_motor, motor &right_back_motor,
-                   int &speed) {
-  left_front_motor.spin(directionType::fwd, speed, percentUnits::pct);
-  right_front_motor.spin(directionType::fwd, speed, percentUnits::pct);
-  left_back_motor.spin(directionType::fwd, speed, percentUnits::pct);
-  right_back_motor.spin(directionType::fwd, speed, percentUnits::pct);
-}
+                   int &speed);
 
 void drive_backward(motor &left_front_motor, motor &left_back_motor,
                     motor &right_front_motor, motor &right_back_motor,
-                    int speed) {
-  left_front_motor.spin(directionType::rev, speed, percentUnits::pct);
-  right_front_motor.spin(directionType::rev, speed, percentUnits::pct);
-  left_back_motor.spin(directionType::rev, speed, percentUnits::pct);
-  right_back_motor.spin(directionType::rev, speed, percentUnits::pct);
-}
+                    int speed);
 
 void turn_left(motor &left_front_motor, motor &left_back_motor,
-               motor &right_front_motor, motor &right_back_motor, int speed) {
-  left_front_motor.spin(directionType::rev, speed, percentUnits::pct);
-  right_front_motor.spin(directionType::fwd, speed, percentUnits::pct);
-  left_back_motor.spin(directionType::rev, speed, percentUnits::pct);
-  right_back_motor.spin(directionType::fwd, speed, percentUnits::pct);
-}
+               motor &right_front_motor, motor &right_back_motor, int speed);
 
 void turn_right(motor &left_front_motor, motor &left_back_motor,
-                motor &right_front_motor, motor &right_back_motor, int speed) {
-  left_front_motor.spin(directionType::fwd, speed, percentUnits::pct);
-  right_front_motor.spin(directionType::rev, speed, percentUnits::pct);
-  left_back_motor.spin(directionType::fwd, speed, percentUnits::pct);
-  right_back_motor.spin(directionType::rev, speed, percentUnits::pct);
-}
+                motor &right_front_motor, motor &right_back_motor, int speed);
