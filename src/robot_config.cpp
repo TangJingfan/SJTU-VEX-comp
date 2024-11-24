10a3e709ea535db7cc7e1cef6221992b78e85a70
TangWan <tangjingfan@gmail.com>
2024-11-24 22:48:41 +0800

    fix: no stop in usercontrol.cpp

--- src/robot_config.cpp
+++ src/robot_config.cpp
@@ -10,9 +10,9 @@ competition Competition;
 controller Controller;
 
 // Motor setup
-motor left_front_motor(PORT1, gearSetting::ratio18_1, false);
-motor right_front_motor(PORT2, gearSetting::ratio18_1, true);
-motor left_back_motor(PORT3, gearSetting::ratio18_1, false);
+motor left_front_motor(PORT12, gearSetting::ratio18_1, false);
+motor right_front_motor(PORT3, gearSetting::ratio18_1, true);
+motor left_back_motor(PORT11, gearSetting::ratio18_1, false);
 motor right_back_motor(PORT4, gearSetting::ratio18_1, true);
 
 // inertial sensor setup
