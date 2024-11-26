a8bf4279fa274936adddb0bca60167585f702f10
TangWan <tangjingfan@gmail.com>
2024-11-26 21:16:25 +0800

    feat: add interface for auto mode

--- include/movement.h
+++ include/movement.h
@@ -14,10 +14,10 @@ void turn_right_control(const double &voltage);
 
 void drive_backward_auto(const double &voltage);
 
-void turn_left_auto_certain_degree(double &voltage,
-                                   const double &target_degree);
-
-// void turn_right_auto_certain_degree(double& voltage, const double&
-// target_degree);
-
 void stop(brakeType b_type);
+
+void move_certain_distance(int tr_distance);
+
+void turn_certain_degree(int tr_degree);
+
+double distance_to_degree(double distance);
