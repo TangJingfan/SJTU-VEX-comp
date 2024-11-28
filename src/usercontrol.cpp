431973409c5fa0fe6ab7beb9b0250b5b3530afa5
tang_jingfan <tangjingfan@gmail.com>
2024-11-28 20:37:41 +0800

    fix: code format

--- src/usercontrol.cpp
+++ src/usercontrol.cpp
@@ -1,6 +1,5 @@
 #include "usercontrol.h"
 
-
 using namespace vex;
 
 // kP, ki, kd need to be modified for different balls
@@ -22,8 +21,6 @@ void usercontrol() {
     // Get button information
     bool whether_shoot = Controller.ButtonL1.pressing();
     bool whether_intake = Controller.ButtonR1.pressing();
-
-
 
     // Scale axis values to motor voltage
     double forward_backward_voltage;
@@ -68,32 +65,20 @@ void usercontrol() {
       }
     }
 
-    //intake  (keep pressing R1 button)
-    if(whether_intake)
-    {
+    // intake  (keep pressing R1 button)
+    if (whether_intake) {
       intake(MAXMOTOR_VOL);
-    }
-    else
-    {
+    } else {
       stop_intake();
     }
 
-
-
-    //shoot   (keep pressing L1 button)
-    if(whether_shoot)
-    {
+    // shoot   (keep pressing L1 button)
+    if (whether_shoot) {
       flywheel.set_target_voltage(10000);
-      flywheel.maintain_woltage();
-    }
-    else
-    {
+      flywheel.maintain_voltage();
+    } else {
       flywheel.stop();
     }
-
-
-
-
 
     // Delay for task scheduler
     vex::task::sleep(20);
