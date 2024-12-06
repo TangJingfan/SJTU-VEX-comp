e831edf53fa5065cddc026c49bdde6e5b1e6ad01
Daicheng Li <not_lell@sjtu.edu.cn>
2024-12-06 15:53:17 +0800

    feat: change port

--- src/robot_config.cpp
+++ src/robot_config.cpp
@@ -10,22 +10,22 @@ competition Competition;
 controller Controller;
 
 // Motor setup
-motor left_front_motor(PORT12, gearSetting::ratio18_1, false);
-motor right_front_motor(PORT3, gearSetting::ratio18_1, true);
-motor left_back_motor(PORT11, gearSetting::ratio18_1, false);
-motor right_back_motor(PORT4, gearSetting::ratio18_1, true);
+motor left_front_motor(PORT10, gearSetting::ratio18_1, false);
+motor right_front_motor(PORT6, gearSetting::ratio18_1, true);
+motor left_back_motor(PORT19, gearSetting::ratio18_1, false);
+motor right_back_motor(PORT13, gearSetting::ratio18_1, true);
 
 // Flywheel
-motor flywheel_motor(PORT10, gearSetting::ratio18_1, false);
+motor flywheel_motor(PORT1, gearSetting::ratio18_1, false);
 
 // Intaker
-motor intaker_motor(PORT15, gearSetting::ratio18_1, false);
+motor intaker_motor(PORT2, gearSetting::ratio18_1, false);
 
 // Transmitter
-motor transmitter_motor(PORT16, gearSetting::ratio18_1, false);
+motor transmitter_motor(PORT3, gearSetting::ratio18_1, false);
 
 // inertial sensor setup
-inertial inertial_sensor(PORT5);
+inertial inertial_sensor(PORT9);
 
 // Brain for feedback
 brain Brain;
