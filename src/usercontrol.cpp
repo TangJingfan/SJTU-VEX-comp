3e1f08028d9fd80c006b22e15017f2371c554bb0
Daicheng Li <not_lell@sjtu.edu.cn>
2024-12-06 23:20:21 +0800

    feat: add reverse control

--- src/usercontrol.cpp
+++ src/usercontrol.cpp
@@ -14,9 +14,14 @@ void usercontrol() {
     const double min_voltage = 2400.0;
 
     while (true) {
-        // Get controller axis values
-        double forward_backward_axis_value = Controller.Axis3.position();
-        double left_right_axis_value = Controller.Axis4.position();
+        // Get controller axis values(intaker as head)
+        double forward_backward_axis_value_1 = Controller.Axis3.position();
+        double left_right_axis_value_1 = Controller.Axis4.position();
+        // Get controller axis values(lifter as head)
+        double forward_backward_axis_value_2 = Controller.Axis2.position();
+        double left_right_axis_value_2 = Controller.Axis1.position();
+
+
         // Get button information
         bool whether_shoot = Controller.ButtonL1.pressing();
         bool whether_intake = Controller.ButtonR1.pressing();
@@ -25,43 +30,104 @@ void usercontrol() {
         double forward_backward_voltage;
         double left_right_voltage;
         // Scale axis values to motor voltage
-        if (forward_backward_axis_value > DEADZONE &&
-            forward_backward_axis_value < threshold_for_movement) {
+        if (forward_backward_axis_value_1 > DEADZONE &&
+            forward_backward_axis_value_1 < threshold_for_movement) {
             forward_backward_voltage = min_voltage;
         } else {
-            forward_backward_voltage = (forward_backward_axis_value * max_voltage) / 100;
+            forward_backward_voltage = (forward_backward_axis_value_1 * max_voltage) / 100;
         }
-        if (left_right_axis_value > DEADZONE &&
-            left_right_axis_value < threshold_for_movement) {
+        if (left_right_axis_value_1 > DEADZONE &&
+            left_right_axis_value_1 < threshold_for_movement) {
             left_right_voltage = min_voltage;
         } else {
-            left_right_voltage = (left_right_axis_value * max_voltage) / 100;
+            left_right_voltage = (left_right_axis_value_1 * max_voltage) / 100;
         }
+        if (forward_backward_axis_value_2 > DEADZONE &&
+            forward_backward_axis_value_2 < threshold_for_movement) {
+            forward_backward_voltage = min_voltage;
+        } else {
+            forward_backward_voltage = (forward_backward_axis_value_2 * max_voltage) / 100;
+        }
+        if (left_right_axis_value_2 > DEADZONE &&
+            left_right_axis_value_2 < threshold_for_movement) {
+            left_right_voltage = min_voltage;
+        } else {
+            left_right_voltage = (left_right_axis_value_2 * max_voltage) / 100;
+        }
+
+
         // Check for DEADZONE
-        bool within_DEADZONE = fabs(forward_backward_axis_value) < DEADZONE &&
-                               fabs(left_right_axis_value) < DEADZONE;
+        bool within_DEADZONE_1 = fabs(forward_backward_axis_value_1) < DEADZONE &&
+                               fabs(left_right_axis_value_1) < DEADZONE;
         // control part
-        if (within_DEADZONE) {
+        if (within_DEADZONE_1) {
             // Stop all motors if within the DEADZONE
             stop(coast);
         } else {
             // Determine movement based on joystick inputs
-            if (fabs(forward_backward_axis_value) >= fabs(left_right_axis_value)) {
+            if (fabs(forward_backward_axis_value_1) >= fabs(left_right_axis_value_1)) {
                 // Forward or backward movement
-                if (forward_backward_axis_value > 0) {
+                if (forward_backward_axis_value_1 > 0) {
                     drive_forward_control(forward_backward_voltage);
                 } else {
                     drive_backward_control(-forward_backward_voltage);
                 }
             } else {
                 // Turning movement
-                if (left_right_axis_value > 0) {
+                if (left_right_axis_value_1 > 0) {
                     turn_right_control(left_right_voltage);
                 } else {
                     turn_left_control(-left_right_voltage);
                 }
             }
         }
+
+
+        // Check for DEADZONE
+        bool within_DEADZONE_2 = fabs(forward_backward_axis_value_2) < DEADZONE &&
+                               fabs(left_right_axis_value_2) < DEADZONE;
+        // control part
+        if (within_DEADZONE_2) {
+            // Stop all motors if within the DEADZONE
+            stop(coast);
+        } else {
+            // Determine movement based on joystick inputs
+            if (fabs(forward_backward_axis_value_2) >= fabs(left_right_axis_value_2)) {
+                // Forward or backward movement
+                if (forward_backward_axis_value_2 > 0) {
+                    drive_backward_control(forward_backward_voltage);
+                } else {
+                    drive_forward_control(-forward_backward_voltage);
+                }
+            } else {
+                // Turning movement
+                if (left_right_axis_value_2 > 0) {
+                    turn_right_control(left_right_voltage);
+                } else {
+                    turn_left_control(-left_right_voltage);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // intake (keep pressing R1 button)
         if (whether_intake) {
