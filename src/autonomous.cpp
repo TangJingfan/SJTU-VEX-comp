d8305a39e9522ad1c240aa00666525477ba4df76
Daicheng Li <not_lell@sjtu.edu.cn>
2024-12-07 01:16:34 +0800

    fix:auto

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -8,9 +8,10 @@ void autonomous() {
 
 
   //first push ball into circle
-  move_certain_forward_distance(1219.2,6000);
+  move_certain_forward_distance(1419.2,6000);
   turn_right_certain_degree(30,4000);
-  move_certain_backward_distance(800,6000);
+  move_certain_backward_distance(1800,6000);
+  move_certain_forward_distance(200,6000);
 
 
   //then move to the other balls and shoot them
